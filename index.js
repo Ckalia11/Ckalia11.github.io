3acd551d1949bc14d79dd6e50cd87779f67e09b4
Chetan <ckalia@uwaterloo.ca>
2023-09-20 16:23:10 -0400

    made about page as default active navlink

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ navLinks.forEach((link) => {
     link.classList.add('active-link');
   }
 });
+
+if (currentPageUrl.endsWith('/index.html')) {
+  const aboutLink = document.querySelector('nav ul li a[href="index.html"]');
+  if (aboutLink) {
+    aboutLink.classList.add('active-link');
+  }
+}
